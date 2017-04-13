06e573a58fc5b909c5e32716eaa61d1b64a72821
kunal_patel <kunal_patel@symantec.com>
2017-04-13 11:49:44 +0530

    ignore and trying out something

--- src/test/java/org/coderearth/kitchen/RemoteServiceClientTest.java
+++ src/test/java/org/coderearth/kitchen/RemoteServiceClientTest.java
@@ -3,12 +3,11 @@ package org.coderearth.kitchen;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by kunal_patel on 4/13/17.
